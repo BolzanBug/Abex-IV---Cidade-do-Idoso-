e3f55c40d0cd422652ba4179499a04616d929dae
gabrielReisPR <gabrielreis0506@gmail.com>
2025-10-18 16:57:02 -0300

    Criação do ambiente completo back e front, com docker

---
+++ backend/src/main/java/br/com/cidadedoidoso/api_idosos/ApiIdososApplication.java
@@ -0,0 +1,13 @@
+package br.com.cidadedoidoso.api_idosos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIdososApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIdososApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/cidadedoidoso/api_idosos/ApiIdososApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cidadedoidoso.api_idosos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIdososApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
