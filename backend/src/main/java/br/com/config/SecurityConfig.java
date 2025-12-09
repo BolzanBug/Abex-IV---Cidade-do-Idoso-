a3d2c64b9899fb2828da23754bf95172dfb63703
GustavoAlderi <gustavoputon1@gmail.com>
2025-12-09 19:03:17 -0300

    Update SecurityConfig.java

--- backend/src/main/java/br/com/config/SecurityConfig.java
+++ backend/src/main/java/br/com/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.cidadedoidoso.api_idosos.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,16 +13,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable()) // Permite requisições POST no Postman
+                .csrf(csrf -> csrf.disable()) // Desativa CSRF para permitir POST no Postman
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/login/**",
                                 "/idosos/**"
-                        ).permitAll()  // LIBERA cadastro e login
-                        .anyRequest().authenticated() // bloqueia o resto
+                        ).permitAll() // LIBERA login e cadastro
+                        .anyRequest().authenticated() // protege o resto
                 )
-                .httpBasic(httpSec -> httpSec.disable()) // desativa login básico
-                .formLogin(form -> form.disable()); // desativa formulário do Spring
+
+                .httpBasic(httpSec -> httpSec.disable()) // desativa auth padrão
+                .formLogin(form -> form.disable()); // desativa formulário
 
         return http.build();
     }
