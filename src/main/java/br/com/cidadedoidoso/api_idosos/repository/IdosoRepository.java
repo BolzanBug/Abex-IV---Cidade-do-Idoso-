b12cae2f5df02694ae807900a3c272d2e661c96f
GustavoAlderi <gustavoputon1@gmail.com>
2025-12-08 22:41:55 -0300

    vai na fé

--- src/main/java/br/com/cidadedoidoso/api_idosos/repository/IdosoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.cidadedoidoso.api_idosos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.cidadedoidoso.api_idosos.model.Idoso;
-
-public interface IdosoRepository extends JpaRepository<Idoso, Long> {
-    boolean existsByCpf(String cpf);
-    boolean existsByEmail(String email);  // Adicionado
-}
