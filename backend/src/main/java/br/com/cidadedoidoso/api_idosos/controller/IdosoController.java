27687e5ce2164fd84c013f0d6331eed1a88d0080
Rafael Almeida <rafaalmeidamachado2017@gmail.com>
2025-12-10 01:42:51 -0300

    Revert "Merge branch 'develop' of https://github.com/BolzanBug/Abex-IV---Cidade-do-Idoso- into develop"
    This reverts commit c805af3896de041fca2c0adb154754e7ff2b9541, reversing
    changes made to 30abc20f8026fa1ea646590790d4907b1c7ac530.

--- backend/src/main/java/br/com/cidadedoidoso/api_idosos/controller/IdosoController.java
+++ backend/src/main/java/br/com/cidadedoidoso/api_idosos/controller/IdosoController.java
@@ -1,32 +1,24 @@
 package br.com.cidadedoidoso.api_idosos.controller;
 
 import br.com.cidadedoidoso.api_idosos.banco_de_dados.entities.Idoso;
-import br.com.cidadedoidoso.api_idosos.dto.IdosoDTO;
-import br.com.cidadedoidoso.api_idosos.service.IdosoService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import br.com.cidadedoidoso.api_idosos.banco_de_dados.repositories.IdosoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/idosos")
-@CrossOrigin(origins = "*") // mantém se sua aplicação precisar aceitar requisições externas
-@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class IdosoController {
 
-    private final IdosoService idosoService;
-
-    @PostMapping
-    public ResponseEntity<Idoso> cadastrar(@RequestBody @Valid IdosoDTO dto) {
-        Idoso salvo = idosoService.cadastrar(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(salvo);
-    }
+    @Autowired
+    private IdosoRepository idosoRepository;
 
     @GetMapping
-    public ResponseEntity<List<Idoso>> listar() {
-        return ResponseEntity.ok(idosoService.listar());
+    public List<Idoso> listarIdosos() {
+        return idosoRepository.findAll();
     }
 }
+
+//ROTA PARA CHAMAR A LISTAGEM "http://localhost:8080/idosos"
