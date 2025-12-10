fd51a556ab8c04b67cf462a1bdaf13c246de0050
GustavoAlderi <gustavoputon1@gmail.com>
2025-12-10 02:50:10 -0300

    deletado dupilicata securityconfig

--- backend/src/main/java/br/com/cidadedoidoso/api_idosos/banco_de_dados/entities/Idoso.java
+++ backend/src/main/java/br/com/cidadedoidoso/api_idosos/banco_de_dados/entities/Idoso.java
@@ -1,15 +1,15 @@
 package br.com.cidadedoidoso.api_idosos.banco_de_dados.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "idosos")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Idoso {
 
     @Id
@@ -18,7 +18,6 @@ public class Idoso {
 
     private String nome;
 
-    @Column(unique = true)
     private String email;
 
     @Column(unique = true)
